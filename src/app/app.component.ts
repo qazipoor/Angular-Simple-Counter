e60df58db4d94c3f5d38a3275f6ee3da05fc6834
Ali Qazipoor <qazipoor@gmail.com>
2022-08-25 20:54:42 +0300

    Objects, methods added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <div id="mainArea">
+    <p>Counter: <span>{{ counter }}</span></p>
+    <button id="mainButton" (click)="increment()">Hit to increase</button>
+  </div>
+  `,
+  styles: ['']
 })
 export class AppComponent {
   title = 'Angular-Simple-Counter';
+  counter: number = 0;
+
+  increment() {
+    this.counter++;
+  }
 }
